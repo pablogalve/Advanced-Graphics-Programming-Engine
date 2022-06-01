bace008217fcdbf70f262c26126266b8e5ed20de
Germán Insua <1996GInsua@gmail.com>
2022-06-01 18:49:11 +0200

    Testing skybox

--- Engine/Code/engine.cpp
+++ Engine/Code/engine.cpp
@@ -714,6 +714,7 @@ void Render(App* app)
 
     RenderSkybox(app);
 
+
     glBindTexture(GL_TEXTURE_2D, 0);
     glUseProgram(0);
 }
@@ -766,52 +767,58 @@ void InitSkybox(App* app)
     glUniform1i(glGetUniformLocation(app->skybox.shaderId, "skybox"), 0);
 
     app->skybox.skyboxVertices = {
-        // Coordinates    
-        -1.0f, -1.0f,  1.0f,//       7--------6
-         1.0f, -1.0f,  1.0f,//      /|       /|
-         1.0f, -1.0f, -1.0f,//     4--------5 |
-        -1.0f, -1.0f, -1.0f,//     | |      | |
-        -1.0f,  1.0f,  1.0f,//     | 3------|-2
-         1.0f,  1.0f,  1.0f,//     |/       |/
-         1.0f,  1.0f, -1.0f,//     0--------1
-        -1.0f,  1.0f, -1.0f,
-    };
+        // positions          
+     -1.0f,  1.0f, -1.0f,
+     -1.0f, -1.0f, -1.0f,
+      1.0f, -1.0f, -1.0f,
+      1.0f, -1.0f, -1.0f,
+      1.0f,  1.0f, -1.0f,
+     -1.0f,  1.0f, -1.0f,
 
-    app->skybox.skyboxIndices = {
-        // Right
-        1, 2, 6,
-        6, 5, 1,
-        // Left
-        0, 4, 7,
-        7, 3, 0,
-        // Top
-        4, 5, 6,
-        6, 7, 4,
-        // Bottom
-        0, 3, 2,
-        2, 1, 0,
-        // Back
-        0, 1, 5,
-        5, 4, 0,
-        // Front
-        3, 7, 6,
-        6, 2, 3
+     -1.0f, -1.0f,  1.0f,
+     -1.0f, -1.0f, -1.0f,
+     -1.0f,  1.0f, -1.0f,
+     -1.0f,  1.0f, -1.0f,
+     -1.0f,  1.0f,  1.0f,
+     -1.0f, -1.0f,  1.0f,
+
+      1.0f, -1.0f, -1.0f,
+      1.0f, -1.0f,  1.0f,
+      1.0f,  1.0f,  1.0f,
+      1.0f,  1.0f,  1.0f,
+      1.0f,  1.0f, -1.0f,
+      1.0f, -1.0f, -1.0f,
+
+     -1.0f, -1.0f,  1.0f,
+     -1.0f,  1.0f,  1.0f,
+      1.0f,  1.0f,  1.0f,
+      1.0f,  1.0f,  1.0f,
+      1.0f, -1.0f,  1.0f,
+     -1.0f, -1.0f,  1.0f,
+
+     -1.0f,  1.0f, -1.0f,
+      1.0f,  1.0f, -1.0f,
+      1.0f,  1.0f,  1.0f,
+      1.0f,  1.0f,  1.0f,
+     -1.0f,  1.0f,  1.0f,
+     -1.0f,  1.0f, -1.0f,
+
+     -1.0f, -1.0f, -1.0f,
+     -1.0f, -1.0f,  1.0f,
+      1.0f, -1.0f, -1.0f,
+      1.0f, -1.0f, -1.0f,
+     -1.0f, -1.0f,  1.0f,
+      1.0f, -1.0f,  1.0f
     };
 
     // skybox VAO
     glGenVertexArrays(1, &app->skybox.VAO);
     glGenBuffers(1, &app->skybox.VBO);
-    glGenBuffers(1, &app->skybox.EBO);
     glBindVertexArray(app->skybox.VAO);
     glBindBuffer(GL_ARRAY_BUFFER, app->skybox.VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(app->skybox.skyboxVertices), &app->skybox.skyboxVertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, app->skybox.EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(app->skybox.skyboxIndices), &app->skybox.skyboxIndices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(&app->skybox.skyboxVertices), &app->skybox.skyboxVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     // load textures
     // -------------
@@ -851,7 +858,7 @@ void RenderSkybox(App* app)
     glBindVertexArray(app->skybox.VAO);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_CUBE_MAP, app->skybox.cubemapTexture);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
     glBindVertexArray(0);
 
     glDepthFunc(GL_LESS); // set depth function back to default
@@ -860,6 +867,7 @@ void RenderSkybox(App* app)
     {
         glPopDebugGroup();
     }
+
 }
 
 unsigned int loadCubemap(std::vector<std::string> faces)
